8628a7327d903794f7d348fdfabf1cf127e7ef4f
matespinosa <matespinosa09@gmail.com>
2020-12-11 18:04:05 -0500

    añadiendo dist

---
+++ dist/precache-manifest.eff1e53638cae7f9054d10a597269d23.js
@@ -0,0 +1,39 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d91f9ed28fbabc14064",
+    "url": "/1.app.bundle.js"
+  },
+  {
+    "revision": "7f43483d1ab689ba6f8e",
+    "url": "/2.app.bundle.js"
+  },
+  {
+    "revision": "dc4262530c472ef92065",
+    "url": "/app.bundle.js"
+  },
+  {
+    "url": "/icon_128x128.6604c172e8ce4af7fbfa57c27d021c51.png"
+  },
+  {
+    "url": "/icon_192x192.90b0066825ca5de6736df558ac2e3d28.png"
+  },
+  {
+    "url": "/icon_256x256.ec379c2f555469a2eae002ecb797de61.png"
+  },
+  {
+    "url": "/icon_384x384.1b3dd1daeb0fae568378c939c236d79b.png"
+  },
+  {
+    "url": "/icon_512x512.6347ff3bd381b29aff5d609e848eb7d9.png"
+  },
+  {
+    "url": "/icon_96x96.ea8ccf240813799ababb948f696bab65.png"
+  },
+  {
+    "revision": "ddf6573253966155b9df025bbf1cb706",
+    "url": "/index.html"
+  },
+  {
+    "url": "/manifest.9338a9d1dc247a46988be67bd70f0577.json"
+  }
+]);
